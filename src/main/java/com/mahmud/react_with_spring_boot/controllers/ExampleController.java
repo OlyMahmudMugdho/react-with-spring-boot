ae3eb2663335d62e1b9bfadd0e7f94d1d46aa924
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-08-09 20:32:24 +0600

    added CrossOrigin header

--- src/main/java/com/mahmud/react_with_spring_boot/controllers/ExampleController.java
+++ src/main/java/com/mahmud/react_with_spring_boot/controllers/ExampleController.java
@@ -2,12 +2,14 @@ package com.mahmud.react_with_spring_boot.controllers;
 
 import com.mahmud.react_with_spring_boot.model.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@CrossOrigin(originPatterns = "*")
+@RequestMapping("")
 public class ExampleController {
     @GetMapping()
     public ResponseEntity<Response> index() {
